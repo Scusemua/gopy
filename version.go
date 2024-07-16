a49de670cec60db07ce7aa31eb8d3fcdc4dc8eea
Ben Carver <bcarver2@gmu.edu>
2024-07-16 17:44:56 +0000

    v0.4.11 release

--- version.go
+++ version.go
@@ -4,6 +4,6 @@ package main
 
 const (
 	Version     = "v0.4.11"
-	GitCommit   = "be0d7e1"          // the commit JUST BEFORE the release
-	VersionDate = "2024-07-16 17:40" // UTC
+	GitCommit   = "f8b0a60"          // the commit JUST BEFORE the release
+	VersionDate = "2024-07-16 17:44" // UTC
 )
